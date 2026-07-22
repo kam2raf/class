cd140e7d7bdc3eb80aae8afdc6ac6304d52316f0
kam2raf <kam2raf@ukr.net>
2026-07-22 19:02:14 +0300

    Add sample chess games to sample_games.py

---
+++ sample_games.py
@@ -0,0 +1,30 @@
+SAMPLE_GAMES = [
+
+"""
+[Event "Demo"]
+
+1.e4 e5
+2.Nf3 Nc6
+3.Bc4 Bc5
+4.c3 Nf6
+5.d4
+""",
+
+"""
+[Event "Demo"]
+
+1.e4 c5
+2.Nf3 d6
+3.d4 cxd4
+4.Nxd4
+""",
+
+"""
+[Event "Demo"]
+
+1.d4 d5
+2.c4
+3.Nc3
+4.Nf3
+"""
+]
